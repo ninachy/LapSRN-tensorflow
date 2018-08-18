06db0b55a78bf18c8cde6b334d3d67daaf1d1b28
ninachy <qian.yue1993@hotmail.com>
2018-08-18 16:18:58 +0800

    initial

--- config.py
+++ config.py
@@ -4,20 +4,20 @@ import json
 config = edict()
 
 config.model = edict()
-config.model.result_path = "samples"
-config.model.checkpoint_path = "checkpoint"
-config.model.log_path = "log"
+config.model.result_path = "debug/samples"
+config.model.checkpoint_path = "debug/checkpoint"
+config.model.log_path = "debug/log"
 config.model.scale = 4
 config.model.resblock_depth  = 10
 config.model.recursive_depth = 1
 
 config.valid = edict()
-config.valid.hr_folder_path = '/media/zhehu/DATA/Public_Dataset/NTIRE2017/DIV2K_valid_HR/'
-config.valid.lr_folder_path = '/media/zhehu/DATA/Public_Dataset/NTIRE2017/DIV2K_valid_LR_bicubic/X4/'
+config.valid.hr_folder_path = '/public/qianyue/LapSRN/dataset/DIV2K_valid_HR/'
+config.valid.lr_folder_path = '/public/qianyue/LapSRN/dataset/DIV2K_valid_LR_bicubic/X4/'
 
 config.train = edict()
-config.train.hr_folder_path = '/media/zhehu/DATA/Public_Dataset/NTIRE2017/DIV2K_train_HR/'
-config.train.lr_folder_path = '/media/zhehu/DATA/Public_Dataset/NTIRE2017/DIV2K_train_LR_bicubic/X4/'
+config.train.hr_folder_path = '/public/qianyue/LapSRN/dataset/DIV2K_train_HR/'
+config.train.lr_folder_path = '/public/qianyue/LapSRN/dataset/DIV2K_train_LR_bicubic/X4/'
 config.train.batch_size = 4 # use large number if you have enough memory
 config.train.in_patch_size = 64
 config.train.out_patch_size = config.model.scale * config.train.in_patch_size
